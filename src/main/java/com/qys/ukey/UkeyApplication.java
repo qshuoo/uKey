94bc68c8fac0408ef464dd0e23671141d3cba9d9
qiaoyongshuo <qiaoyongshuo@meituan.com>
2018-07-28 17:24:16 +0800

    feature:init

---
+++ src/main/java/com/qys/ukey/UkeyApplication.java
@@ -0,0 +1,11 @@
+package com.qys.ukey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UkeyApplication.class, args);
+    }
+}
